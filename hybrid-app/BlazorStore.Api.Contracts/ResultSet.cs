b02dfaa7abc4be348727ca6efecdc81c8099ec60
Connor Schmidt <connor_schmidt@trekbikes.com>
2022-07-21 10:50:40 -0500

    Copy sample-app

---
+++ hybrid-app/BlazorStore.Api.Contracts/ResultSet.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorStore.Api.Contracts;
+
+public class ResultSet<T>
+{
+    public List<T> Items { get; }
+    public int TotalCount { get; }
+
+    public ResultSet(List<T> items, int totalCount)
+    {
+        Items = items;
+        TotalCount = totalCount;
+    }
+}
---
+++ hybrid-app/BlazorStore.Client.Web/Program.cs
@@ -0,0 +1,17 @@
+using BlazorStore.Client.Web;
+using BlazorStore.Client.Web.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
+
+builder.Services.AddSingleton<CartStateContainer>();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7278") });
+
+await builder.Build().RunAsync();

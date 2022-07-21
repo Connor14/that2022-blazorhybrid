c2cb9d0f0dcfaa773436005a81e611c7ad9f606e
Connor Schmidt <connor_schmidt@trekbikes.com>
2022-07-21 12:19:27 -0500

    Add shared entrypoint to Web project

--- hybrid-app/BlazorStore.Client.Web/Program.cs
+++ hybrid-app/BlazorStore.Client.Web/Program.cs
@@ -1,5 +1,5 @@
 using BlazorStore.Client.Web;
-using BlazorStore.Client.Web.Services;
+using BlazorStore.Client.Shared.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;

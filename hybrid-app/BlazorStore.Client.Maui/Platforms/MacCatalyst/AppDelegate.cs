36ed2d8cd7b5f23221051692400a4a12fa267697
Connor Schmidt <connor_schmidt@trekbikes.com>
2022-07-21 10:57:25 -0500

    Add .NET MAUI project

---
+++ hybrid-app/BlazorStore.Client.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BlazorStore.Client.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
